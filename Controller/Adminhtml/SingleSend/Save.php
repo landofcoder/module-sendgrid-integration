21c150b04577a3da02913f689113bcc15206f7dd
Chính Nguyễn <vanchinhpc9@gmail.com>
2020-03-13 16:17:48 +0700

    create table template version

--- Controller/Adminhtml/SingleSend/Save.php
+++ Controller/Adminhtml/SingleSend/Save.php
@@ -3,7 +3,9 @@
 
 namespace Lof\SendGrid\Controller\Adminhtml\SingleSend;
 
+use Lof\SendGrid\Helper\Data;
 use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Stdlib\DateTime\DateTimeFactory;
 
 /**
  * Class Save
@@ -14,6 +16,10 @@ class Save extends \Magento\Backend\App\Action
 {
 
     protected $dataPersistor;
+    /**
+     * @var DateTimeFactory
+     */
+    private $_dateFactory;
 
     /**
      * @param \Magento\Backend\App\Action\Context $context
@@ -21,9 +27,13 @@ class Save extends \Magento\Backend\App\Action
      */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
+        \Lof\SendGrid\Helper\Data $helper,
+        DateTimeFactory $dateFactory,
         \Magento\Framework\App\Request\DataPersistorInterface $dataPersistor
     ) {
+        $this->_helperdata = $helper;
         $this->dataPersistor = $dataPersistor;
+        $this->_dateFactory = $dateFactory;
         parent::__construct($context);
     }
 
@@ -37,6 +47,7 @@ class Save extends \Magento\Backend\App\Action
         /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
         $resultRedirect = $this->resultRedirectFactory->create();
         $data = $this->getRequest()->getPostValue();
+        $api_key = $this->_helperdata->getSendGridConfig('general','api_key');
         if ($data) {
             $id = $this->getRequest()->getParam('entity_id');
             $model = $this->_objectManager->create(\Lof\SendGrid\Model\SingleSend::class)->load($id);
@@ -45,7 +56,49 @@ class Save extends \Magento\Backend\App\Action
                 return $resultRedirect->setPath('*/*/');
             }
             $model->setData($data);
-
+            $singlesendId = $model->getSinglesendId();
+            $name = $model->getName();
+            $status = $model->getStatus();
+            if($id) {
+                $model->setUpdateDate($this->_dateFactory->create()->gmtDate());
+                $curl = curl_init();
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => "https://api.sendgrid.com/v3/marketing/singlesends/$singlesendId",
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 30,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "PATCH",
+                    CURLOPT_POSTFIELDS => "{\"name\":\"$name\",\"status\":\"$status\",\"id\":\"$singlesendId\"}",
+                    CURLOPT_HTTPHEADER => array(
+                        "authorization: Bearer $api_key"
+                    ),
+                ));
+                $response = curl_exec($curl);
+                $err = curl_error($curl);
+                curl_close($curl);
+            } else {
+                $model->setCreateDate($this->_dateFactory->create()->gmtDate());
+                $model->setUpdateDate($this->_dateFactory->create()->gmtDate());
+                $curl = curl_init();
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => "https://api.sendgrid.com/v3/marketing/singlesends",
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 30,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "POST",
+                    CURLOPT_POSTFIELDS => "{\"name\":\"$name\",\"status\":\"$status\",\"id\":\"$singlesendId\"}",
+                    CURLOPT_HTTPHEADER => array(
+                        "authorization: Bearer $api_key"
+                    ),
+                ));
+                $response = curl_exec($curl);
+                $err = curl_error($curl);
+                curl_close($curl);
+            }
             try {
                 $model->save();
                 $this->messageManager->addSuccessMessage(__('You saved the Singlesend.'));
