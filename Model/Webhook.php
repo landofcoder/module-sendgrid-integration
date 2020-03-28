ad1cce00f69ee236c2d335300cd22d61ea39404f
Chính Nguyễn <vanchinhpc9@gmail.com>
2020-03-28 10:22:27 +0700

    fix bug webhook does not work

--- Model/Webhook.php
+++ Model/Webhook.php
@@ -1,12 +1,9 @@
 <?php
 namespace Lof\SendGrid\Model;
-
 use Magento\Backend\App\Action\Context;
 use Lof\SendGrid\Api\WebhookInterface;
-
 class Webhook implements WebhookInterface
 {
-
     /**
      * @var \Psr\Log\LoggerInterface
      */
@@ -15,7 +12,6 @@ class Webhook implements WebhookInterface
      * @var \Magento\Framework\Webapi\Rest\Request
      */
     private $request;
-
     public function __construct(
         Context $context,
         \Psr\Log\LoggerInterface $logger,
@@ -31,31 +27,47 @@ class Webhook implements WebhookInterface
         $data = file_get_contents("php://input");
         $events = json_decode($data);
         foreach ($events as $item) {
-            if ($item->mc_stats == "singlesend") {
-                $event = $this->eventfactory->create();
-                $event->setEmails($item->email);
-                $event->setTimestamp(date('m/d/Y H:i:s', $item->timestamp));
-                $event->setEvent($item->event);
-                $event->setMcStats($item->mc_stats);
+            $event = $this->eventfactory->create();
+            $event->setEmails($item->email);
+            $event->setTimestamp(date('m/d/Y H:i:s', $item->timestamp));
+            $event->setEvent($item->event);
+            if(isset($item->phase_id)) {
                 $event->setPhaseId($item->phase_id);
-                $event->setSendAt(date('m/d/Y H:i:s', $item->send_at));
-                $event->setSgEventId($item->sg_event_id);
-                $event->setSgMessageId($item->sg_message_id);
-                $event->setSgTemplateId($item->sg_template_id);
-                $event->setSgTemplateName($item->sg_template_name);
-                $event->setSinglesendId($item->singlesend_id);
-                $event->setTemplateId($item->template_id);
-                if (isset($item->ip)) {
-                    $event->setIp($item->ip);
-                }
-                if (isset($item->useragent)) {
-                    $event->setUseragent($item->useragent);
-                }
-                if (isset($item->category)) {
-                    $event->setCategory($item->category);
-                }
-                $event->save();
             }
+            if(isset($item->send_at)) {
+                $event->setSendAt(date('m/d/Y H:i:s', $item->send_at));
+            }
+            if (isset($item->sg_event_id)) {
+                $event->setSgEventId($item->sg_event_id);
+            }
+            if (isset($item->sg_message_id)) {
+                $event->setSgMessageId($item->sg_message_id);
+            }
+            if (isset($item->sg_template_id)) {
+                $event->setSgTemplateId($item->sg_template_id);
+            }
+            if (isset($item->sg_template_name)) {
+                $event->setSgTemplateName($item->sg_template_name);
+            }
+            if (isset($item->singlesend_id)) {
+                $event->setSinglesendId($item->singlesend_id);
+            }
+            if (isset($item->template_id)) {
+                $event->setTemplateId($item->template_id);
+            }
+            if (isset($item->mc_stats)) {
+                $event->setMcStats($item->mc_stats);
+            }
+            if (isset($item->ip)) {
+                $event->setIp($item->ip);
+            }
+            if (isset($item->useragent)) {
+                $event->setUseragent($item->useragent);
+            }
+            if (isset($item->category)) {
+                $event->setCategory(json_encode($item->category));
+            }
+            $event->save();
         }
     }
 }
